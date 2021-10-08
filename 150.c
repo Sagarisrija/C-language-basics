0208e9899ac9ed2d6212c2b24a113758739a8f85
Sagarisrija <92150198+Sagarisrija@users.noreply.github.com>
2021-10-08 14:07:40 +0530

    Add files via upload

---
+++ 150.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b;
+	b=a--;
+	printf("%d\n",b);
+	printf("%d",a);
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int h,a,b,A;
+	scanf("%d%d%d",&a,&b,&h);
+	A=(h*(a+b))/2;
+	printf("%d",A);
+}
