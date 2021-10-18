8a432034da79c95e5fec5354a935ec1245ec79aa
Sagarisrija <92150198+Sagarisrija@users.noreply.github.com>
2021-10-18 14:15:02 +0530

    Add files via upload

---
+++ 150MARKS.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int TM,EM,MM,SCM,SOM,Total;
+	float Percentage;
+	scanf("%d%d%d%d%d",&TM,&EM,&MM,&SCM,&SOM);
+	
+	Total=TM+EM+MM+SCM+SOM;
+	Percentage =Total*100/500;
+	printf("Percentage=%f",Percentage);
+	if(Percentage>=90)
+	{
+		printf("A grade");
+	}
+	else if(Percentage>=80 && Percentage<=90)
+	{
+		printf("B grade");
+	}
+	else if(Percentage>=70 && Percentage<=80)
+	{
+		printf("C grade");
+	}
+	else if(Percentage>=60 && Percentage<=70)
+	{
+		printf("D grade");
+	}
+	else if(Percentage>=50 && Percentage<=60)
+	{
+		printf("E grade");
+	}
+	else if(Percentage>=40 && Percentage<=50)
+	{
+		printf("Pass");
+	}
+	else
+	{
+		printf("Fail");
+	}
+}
